16e924704b4d88fab3afc5410f8fb81a64778504
Zhang Tielei <zhangtl@pharmolix.com>
2023-10-29 00:54:57 +0800

    修改蛋白质数据的脚本，改正确

--- aidd/json_data_init_pro.py
+++ aidd/json_data_init_pro.py
@@ -1,43 +1,40 @@
+'''
+正式环境执行：
+python -m aidd.json_data_init_pro --env=prod
+测试环境执行：
+python -m aidd.json_data_init_pro --env=dev
+'''
 import json
-# import pymysql
-from db.mysqlhelper import MySqLHelper
-import common.logger
+from aidd.db.mysqlhelper import MySqLHelper
 
 
 # 读取蛋白质json文件 uniprot_accession.json ，将其插入到数据库表 chatdd_target_protein_info
-with open('F:\\work\\data\\uniprot_accession.json', 'r') as file:
+mysqlhelper = MySqLHelper()
+# target_file_path = '/Users/tielei/Documents/project/pharmolix/aidd_data_processing/uniprot_accession_part.json'
+target_file_path = '/root/tielei/code/chatdd-aidd/files/uniprot_accession.json'
+with open(target_file_path, 'r') as file:
     data = json.load(file)
+    target_count = 0
+    insert_count = 0
 
+    for uniprot_id, properties in data.items():
+        names = properties['Name'] #需要注意：Name节点是一个list
+        entry_name = properties['Entry_Name']
+        sequence = properties['Sequence']
+        if len(sequence) > 2048:
+            print(f"WARN: target {names} / with uniprot id: {uniprot_id} has sequence greater than 2048: {len(sequence)}")
 
+        for name in names:
+            if name and entry_name and sequence:
+                target_count += 1
+                # 打印一下中间结果
+                if target_count % 10000 == 0:
+                    print(f"processed {target_count} target items...")
+                result = mysqlhelper.insertone(
+                    "insert into chatdd_target_protein_info (name, entry_name, sequence) values(%s, %s, %s)",
+                    (name, entry_name, sequence[:2048]))
+                if result:
+                    insert_count +=1
 
-    # 如果数据是一个字典，可以这样迭代
-
-    count = 0;
-
-    if isinstance(data, dict):
-        for key, value in data.items():
-            #print(f"{key}: {value}")
-            for key2, value2 in value.items():
-                if (key2 == 'Name'):         #
-                    # print(f"{value}")
-                    val_name = value2
-                if (key2 == 'Entry_Name'):   #
-                    # print(f"{value}")
-                    val_entry_name = {value2}
-                if (key2 == 'Sequence'):     #
-                    # print(f"{value}")
-                    val_sequence = {value2}
-
-
-
-                    if val_name and val_entry_name and val_sequence :
-                        mysqlhelper = MySqLHelper()
-                        result = mysqlhelper.insertone(
-                            "insert into chatdd_target_protein_info(name, entry_name, sequence) values( % s, % s, % s)",
-                            (val_name, val_entry_name, val_sequence))
-                        # print("result====",result)
-                        count += 1
-                        # print("count:", count)
-
-    print("count:", count)
+print(f"total inserted target/protein count: {insert_count}; total target items in file: {target_count}")
 
